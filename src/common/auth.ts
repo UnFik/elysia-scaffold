0243a565011380d6053ce716c6d59b86283fd430
UnFik <FIKRIILHAMARIFIN_1313621027@mhs.unj.ac.id>
2026-01-12 11:28:21 +0700

    update: google auth provider

--- src/common/auth.ts
+++ src/common/auth.ts
@@ -21,6 +21,12 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
+  socialProviders: {
+    google: { 
+        clientId: process.env.GOOGLE_CLIENT_ID as string, 
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+    }, 
+},
   session: {
     expiresIn: 60 * 60 * 24 * 7,
     updateAge: 60 * 60 * 24,
